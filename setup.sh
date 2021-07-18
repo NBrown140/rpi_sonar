1d70ad215bca9974405b946e346af9ba2acd6714
Nicolas Brown <nic.brown0@gmail.com>
2021-07-18 12:01:11 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+apt-get update -y
+apt-get upgrade -y
+
+# Install gpsd
+apt-get install gpsd gpsd-clients
+
+# Disable default gpsd service
+systemctl disable gpsd.socket
+
+# Start a new gpsd instance that redirects the data of the correct serial port to a socket
+gpsd /dev/serial1 -F /var/run/gpsd.sock
