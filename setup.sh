e617faf42ccf4d7dc8d4e6803201b5d7a7cd7861
Nicolas Brown <nic.brown0@gmail.com>
2021-08-29 21:56:27 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,9 @@
 apt-get update -y
 apt-get upgrade -y
 
-# # Install gpsd
-# apt-get install gpsd gpsd-clients
+# Enable GPIO serial port
+sh -c 'echo "enable_uart=1" >> /boot/config.txt'
 
-# # Disable default gpsd service
-# systemctl disable gpsd.socket
-
-# # Start a new gpsd instance that redirects the data of the correct serial port to a socket
-# gpsd /dev/serial0 -F /var/run/gpsd.sock
+# Disable console serial port
+systemctl stop serial-getty@ttyS0.service
+systemctl disable serial-getty@ttyS0.service
